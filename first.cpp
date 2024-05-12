b83486c0bd7854bb143fcee7ca6d8e702dffbf1e
AndreCorreaSantos <acorreasantos2002@gmail.com>
2024-05-11 21:27:12 -0300

    working route generation

--- first.cpp
+++ first.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 #include <limits>
 #include <map>
-#include <vector>
 #include <set>
+#include <vector>
 using namespace std;
 
 // read stops information from file
@@ -60,25 +60,58 @@ void write_routes(const vector<vector<int>> &routes, const string &filename) { /
 
 // function that receives the number of points and returns all of the possible permutations
 vector<vector<int>> generate_permutations(int num_points) {
-    vector<vector<int>> permutations;  
-    vector<int> route(num_points);
+    vector<vector<int>> permutations;
+    vector<int> route(num_points - 1); // Only permute num_points-1 elements
 
-    // Initialize the route with 0, 1, 2, ..., num_points-1
-    for (int i = 0; i < num_points; i++) {
-        route[i] = i;
+    // Initialize the route with 1, 2, ..., num_points-1
+    for (int i = 1; i < num_points; i++) {
+        route[i - 1] = i;
     }
 
     // Generate permutations using std::next_permutation
-    while (next_permutation(route.begin(), route.end()))
-    {
-        permutations.push_back(route);
-    }
+    do {
+        vector<int> full_route;
+        full_route.push_back(0); // Start with 0
+        full_route.insert(full_route.end(), route.begin(), route.end());
+        full_route.push_back(0); // End with 0
+        permutations.push_back(full_route);
+    } while (next_permutation(route.begin(), route.end()));
 
     return permutations;
 }
+// function that receives the permutations and tries to fix routes that violate constraints, 
+vector<vector<int>> filter_routes(vector<vector<int>> permutations, vector<vector<int>> real_routes, int max_cost) {
 
+    vector<vector<int>> valid_routes;
 
+    for (int r = 0; r < int(permutations.size()); r++) {
+        vector<int> route = permutations[r]; 
+        int cost = 0;
+ 
+        int size = route.size() - 1;
+        bool valid = true;
+        for (int i = 0; i < size; i++) { 
+            int current_stop = route[i];
+            int next_stop = route[i + 1];
 
+            if ((real_routes[current_stop][next_stop] < 1)) { //  if any constraint is violated we try to add 0 in between the edges of the violator to try and fix, ADICIONAR CALCULO DO CUSTO
+
+                if (real_routes[current_stop][0] == 0 || real_routes[0][next_stop] == 0) { // if there isnt a route from current_stop to 0 or from 0 to next stop the route cannot be fixed.
+                    valid = false;
+                    break; // exiting the loop as the current route cannot be fixed
+                }
+
+                route.insert(route.begin()+i+1, 0);
+                size++;
+            }
+        }
+
+        if (valid) {
+            valid_routes.push_back(route);
+        }
+    }
+    return valid_routes;
+}
 
 // // function that receives a graph of routes and returns all possible combinations to traverse the route and go from start, to start
 // vector<vector<vector<int>>> generate_possible_combinations(vector<vector<int>> routes, int max_cost) {
@@ -92,8 +125,6 @@ vector<vector<int>> generate_permutations(int num_points) {
 //     vector<vector<vector<int>>> combinations = generate_possible_combinations(routes);
 // }
 
-
-
 int main(int argc, char *argv[]) {
 
     // reading data
@@ -104,13 +135,18 @@ int main(int argc, char *argv[]) {
     string file_name = argv[1];
     ifstream file(file_name);
     // reading max cost
-    int max_cost; // first line in the file is the max possible cost in the car
-    file >> max_cost;
+    // int max_cost; // first line in the file is the max possible cost in the car
+    // file >> max_cost;
     map<int, int> stops = read_demands(file);                         // store the stops and their demands in the stops map
     vector<vector<int>> routes = read_routes(file, stops.size() + 1); // adding 1 to account for the origin as a vertex in the graph
-    vector<vector<int>> permutation_routes = generate_permutations(routes.size());
+    vector<vector<int>> permutations = generate_permutations(routes.size());
+    vector<vector<int>> valid_routes = filter_routes(permutations,routes,0);
 
-    write_routes(permutation_routes,"debug.txt");
+    write_routes(valid_routes,"debug.txt"); // writing function to debug
+    // cout << permutations.size();
+    // cout << "\n";
+    // cout << valid_routes.size();
+    // cout << "\n";
 
     file.close();
 
