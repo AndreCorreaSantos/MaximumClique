6252f1e11e14aa520d79527750eeb01e912df4a5
AndreCorreaSantos <acorreasantos2002@gmail.com>
2024-06-02 17:27:19 -0300

    fixing local permutations

--- global_omp_mpi.cpp
+++ global_omp_mpi.cpp
@@ -3,16 +3,14 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
-#include "helper.hpp"  // Assuming this contains necessary custom type and function declarations
+#include "helper.hpp"  // Assuming this contains necessary definitions
 
 using namespace std;
-
-
 
 vector<int> serialize_routes(const vector<Route>& routes) {
     vector<int> serialized;
     for (const auto& route : routes) {
-        serialized.push_back(route.stops.size());  // Number of stops
+        serialized.push_back(route.stops.size());
         for (int stop : route.stops) {
             serialized.push_back(stop);
         }
@@ -38,15 +36,15 @@ vector<Route> deserialize_routes(const vector<int>& serialized) {
 
 vector<Route> filter_routes(vector<vector<int>>& permutations, vector<vector<int>>& real_routes, vector<int>& demands, int max_weight, int max_stops) {
     vector<Route> valid_routes;
-    #pragma omp parallel for default(none) shared(valid_routes, permutations, real_routes, demands, max_weight, max_stops)
-    for (int r = 0; r < int(permutations.size()); r++) {
-        vector<int> route = permutations[r];
+    #pragma omp parallel for
+    for (int i = 0; i < permutations.size(); i++) {
+        vector<int> route = permutations[i];
         int cost = 0;
         int weight = 0;
         bool valid = true;
-        for (size_t i = 0; i < route.size() - 1; i++) {
-            int current_stop = route[i];
-            int next_stop = route[i + 1];
+        for (size_t j = 0; j < route.size() - 1; j++) {
+            int current_stop = route[j];
+            int next_stop = route[j + 1];
             int current_cost = real_routes[current_stop][next_stop];
             int next_weight = demands[next_stop];
             if (current_cost == 0 || weight + next_weight > max_weight) {
@@ -71,9 +69,7 @@ int main(int argc, char *argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     if (size < 2) {
-        if (rank == 0) {
-            cerr << "This program needs at least two MPI processes.\n";
-        }
+        cerr << "This program needs at least two MPI processes.\n";
         MPI_Finalize();
         return 1;
     }
@@ -83,51 +79,103 @@ int main(int argc, char *argv[]) {
     vector<vector<int>> permutations;
     int max_weight = 15;
     int max_stops = 7;
+    int permutations_size = 0;
 
     if (rank == 0) {
         ifstream file(argv[1]);
+        if (!file) {
+            cerr << "Failed to open file: " << argv[1] << endl;
+            MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+        }
         demands = read_demands(file);
         route_matrix = read_routes(file, demands.size());
-        permutations = generate_permutations(route_matrix.size());
+        permutations = generate_permutations(demands.size());
+        permutations_size = permutations.size();
     }
 
-    int num_permutations_per_process = permutations.size() / size;
-    vector<vector<int>> local_permutations(num_permutations_per_process);
-    MPI_Scatter(permutations.data(), num_permutations_per_process * permutations[0].size(), MPI_INT,
-                local_permutations.data(), num_permutations_per_process * permutations[0].size(), MPI_INT, 0, MPI_COMM_WORLD);
+    // Broadcast the size of permutations
+    MPI_Bcast(&permutations_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    
+    // Broadcast the size of demands
+    int demands_size = demands.size();
+    MPI_Bcast(&demands_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    vector<Route> local_valid_routes = filter_routes(local_permutations, route_matrix, demands, max_weight, max_stops);
-    vector<int> local_serialized = serialize_routes(local_valid_routes);
-    int local_size = local_serialized.size();
-    vector<int> sizes;
-
-    if (rank == 0) {
-        sizes.resize(size);
+    // Resize vectors in all ranks based on the demands_size
+    if (rank != 0) {
+        demands.resize(demands_size);
+        route_matrix.resize(demands_size, vector<int>(demands_size));
     }
 
-    MPI_Gather(&local_size, 1, MPI_INT, sizes.data(), 1, MPI_INT, 0, MPI_COMM_WORLD);
+    // Broadcast demands
+    MPI_Bcast(demands.data(), demands_size, MPI_INT, 0, MPI_COMM_WORLD);
 
-    vector<int> all_serialized_data;
-    vector<int> displacements;
 
-    if (rank == 0) {
-        displacements.resize(size);
-        int total_size = 0;
-        for (int i = 0; i < size; ++i) {
-            displacements[i] = total_size;
-            total_size += sizes[i];
+    // Broadcast route matrix row by row
+    for (int i = 0; i < demands_size; ++i) {
+        MPI_Bcast(route_matrix[i].data(), demands_size, MPI_INT, 0, MPI_COMM_WORLD);
+    }
+
+    // Debug print to verify broadcasting
+    if (rank == 1) {
+        cout << "Route matrix after broadcasting:" << endl;
+        for (const auto& row : route_matrix) {
+            for (const auto& element : row) {
+                cout << element << " ";
+            }
+            cout << endl;
         }
-        all_serialized_data.resize(total_size);
     }
 
-    MPI_Gatherv(local_serialized.data(), local_size, MPI_INT,
-                all_serialized_data.data(), sizes.data(), displacements.data(), MPI_INT,
-                0, MPI_COMM_WORLD);
+    // Calculate the total number of elements each process will receive
+    int perm_length = demands_size + 1;
+    int num_permutations_per_process = (permutations_size + size - 1) / size;  // Distribute permutations evenly
+    int elements_per_process = num_permutations_per_process * perm_length;
 
+    // Flatten permutations for scattering
+    vector<int> flat_permutations(elements_per_process * size, -1);  // Initialize with -1 to handle cases where there are fewer permutations than needed
     if (rank == 0) {
-        vector<Route> all_valid_routes = deserialize_routes(all_serialized_data);
-        Route cheapest_route = get_cheapest(all_valid_routes);
-        cout << "Cheapest route cost: " << cheapest_route.total_cost << endl;
+        for (int i = 0; i < permutations.size(); ++i) {
+            copy(permutations[i].begin(), permutations[i].end(), flat_permutations.begin() + i * perm_length);
+        }
+    }
+
+    // Debug print to verify flat_permutations on rank 0 before scattering
+    if (rank == 0) {
+        cout << "Flat permutations before scattering:" << endl;
+        for (const auto& perm : flat_permutations) {
+            cout << perm << " ";
+        }
+        cout << endl;
+    }
+
+    // Allocate space for local permutations and scatter
+    vector<int> local_flat_permutations(elements_per_process, -1); // Initialize with -1 to check if values are overwritten
+
+    MPI_Scatter(flat_permutations.data(), elements_per_process, MPI_INT, 
+                local_flat_permutations.data(), elements_per_process, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // Debug print to verify local_flat_permutations on each rank after scattering
+    cout << "Local flat permutations on rank " << rank << ":" << endl;
+    cout << elements_per_process << endl;
+    for (const auto& perm : local_flat_permutations) {
+        cout << perm << " ";
+    }
+    cout << endl;
+
+    // Reshape local_flat_permutations to local_permutations
+    vector<vector<int>> local_permutations(num_permutations_per_process, vector<int>(perm_length));
+    for (int i = 0; i < num_permutations_per_process; ++i) {
+        local_permutations[i].assign(local_flat_permutations.begin() + i * perm_length,
+                                     local_flat_permutations.begin() + (i + 1) * perm_length);
+    }
+
+    // Debug print to verify scattering
+    cout << "Local permutations after scattering on rank " << rank << ":" << endl;
+    for (const auto& perm : local_permutations) {
+        for (const auto& stop : perm) {
+            cout << stop << " ";
+        }
+        cout << endl;
     }
 
     MPI_Finalize();
